021ef359603cc08c0f15fd32c905a7653e73bfa6
Ivan Lupolov <ivannaib2012@gmail.com>
2017-03-23 00:42:22 +0200

    fix method order

--- src/week3/practice2/task2/Desk.java
+++ src/week3/practice2/task2/Desk.java
@@ -27,17 +27,58 @@ public class Desk {
     }
 
     public void order() {
-        int h = 0;
-        for (int j = 0; j < Suit.values.length; j++) {
-            for (int i = 0; i < rusDesk.length; i++) {
-                if (rusDesk[i].getSuit().getName() == Suit.values[j].getName()) {
-                    Card tmp = rusDesk[h];
-                    rusDesk[h] = rusDesk[i];
-                    rusDesk[i] = tmp;
-                    h++;
+        for (int i = 0; i < sizeDesk; i++) {
+            for (int j = 0; j < sizeDesk - 1; j++) {
+                if (compareCard(rusDesk[j],rusDesk[j + 1]) < 0) {
+                    Card tmp = rusDesk[j];
+                    rusDesk[j] = rusDesk[j + 1];
+                    rusDesk[j + 1] = tmp;
                 }
             }
         }
+    }
+
+    public int compareCard(Card c1, Card c2) {
+        int c1Index = 0;
+        int c2Index = 0;
+        int result = 0;
+
+        if(c1.getSuit() == c2.getSuit()) {  // если масти равны, сравниваем по рангу
+
+            if (c1.getRank() == c2.getRank()){ // если карты равны вернем 0
+                return result;
+            }
+
+            for (int i = 0; i < Rank.values.length; i++) {
+                if(c1.getRank() == Rank.values[i]){
+                    c1Index = i;
+                }
+                if (c2.getRank() == Rank.values[i]){
+                    c2Index = i;
+                }
+            }
+            if (c1Index < c2Index) {               // card1 > card2
+                result = 1;
+            } else if (c1Index > c2Index){         // card1 < card2
+                result = -1;
+            }
+            return result;
+        }
+
+        for (int i = 0; i < Suit.values.length; i++) { // сравниваем по масти
+            if(c1.getSuit() == Suit.values[i]){
+                c1Index = i;
+            }
+            if (c2.getSuit() == Suit.values[i]){
+                c2Index = i;
+            }
+        }
+        if (c1Index < c2Index) {               // card1 > card2
+            result = 1;
+        } else if (c1Index > c2Index){         // card1 < card2
+            result = -1;
+        }
+        return result;
     }
 
     public boolean hasNext() {
@@ -48,7 +89,6 @@ public class Desk {
     }
 
     public Card drawOne() {
-
 
         return null;
     }
