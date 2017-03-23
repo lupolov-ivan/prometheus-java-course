7d7287b81134822fcfb57e98ddbd257da5758730
ivan-lupolov <iv.lupolov@gmail.com>
2017-03-23 17:22:48 +0200

    create classes week3 task3

---
+++ src/week3/practice2/task3/Fibonacci.java
@@ -0,0 +1,7 @@
+package src.week3.practice2.task3;
+
+public class Fibonacci {
+    public long getNumber(int position) {
+        return 0;
+    }
+}
---
+++ src/week3/practice2/task3/TestFibonacci.java
@@ -0,0 +1,7 @@
+package src.week3.practice2.task3;
+
+public class TestFibonacci {
+    public static void main(String[] args) {
+
+    }
+}
