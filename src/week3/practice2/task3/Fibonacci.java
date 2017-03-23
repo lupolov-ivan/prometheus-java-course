66b842ad663a7660d31a75666d8630c7d78bef4d
Ivan Lupolov <ivannaib2012@gmail.com>
2017-03-23 22:18:20 +0200

    week3 task3 complete

--- src/week3/practice2/task3/Fibonacci.java
+++ src/week3/practice2/task3/Fibonacci.java
@@ -2,6 +2,12 @@ package src.week3.practice2.task3;
 
 public class Fibonacci {
     public long getNumber(int position) {
-        return 0;
+        if(position <= 0) {
+            return -1;
+        }
+        if (position < 3) {
+            return 1;
+        }
+        return  getNumber(position - 1) + getNumber(position - 2);
     }
 }
--- src/week3/practice2/task3/TestFibonacci.java
+++
@@ -1,7 +0,0 @@
-package src.week3.practice2.task3;
-
-public class TestFibonacci {
-    public static void main(String[] args) {
-
-    }
-}
