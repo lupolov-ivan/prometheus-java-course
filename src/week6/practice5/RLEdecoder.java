3d5621113e021cbc6c8dff78184786c02e585de6
Ivan Lupolov <ivan@freelancehunt.com>
2017-04-26 23:50:28 +0300

    fix and add comments

--- src/week6/practice5/RLEdecoder.java
+++ src/week6/practice5/RLEdecoder.java
@@ -3,18 +3,20 @@ package src.week6.practice5;
 public class RLEdecoder {
     public static void main(String[] args) {
         String str = "f3g5gd5lupolovivan";
-        char ch;
+
         StringBuilder stringBuilder = new StringBuilder();
 
         char[] chars = str.toCharArray();
 
-        for (int i = 0; i < chars.length; i++) {
+        /* add validation for input string*/
+
+        for (int i = 0; i < chars.length; i++) { // алгоритм работает только с валидной строкой
             if (String.valueOf(chars[i]).matches("[0-9]")){
-                for (int j = 0; j < Character.digit(chars[i], 10)-1; j++) {
-                    stringBuilder.append(chars[i-1]);
+                for (int j = 0; j < Character.digit(chars[i], 10) - 1; j++) {
+                    stringBuilder.append(chars[i-1]);  // если следующий символ число, то добавляем в последовательность предыдущий символ (число-1) раз
                 }
             } else {
-                stringBuilder.append(chars[i]);
+                stringBuilder.append(chars[i]); // если нет то просто добавляем символ в последовательность
             }
         }
 
