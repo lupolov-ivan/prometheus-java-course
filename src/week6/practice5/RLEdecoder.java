c364fa5e476dc5d1bdbbed137525111b19f0413a
Ivan Lupolov <ivan@freelancehunt.com>
2017-04-26 23:30:43 +0300

    add v.1 algorithm RLEdecoder

--- src/week6/practice5/RLEdecoder.java
+++ src/week6/practice5/RLEdecoder.java
@@ -2,6 +2,24 @@ package src.week6.practice5;
 
 public class RLEdecoder {
     public static void main(String[] args) {
+        String str = "f3g5gd9y9y9ylupolovivan";
+        char ch;
+        StringBuilder stringBuilder = new StringBuilder();
 
+        char[] chars = str.toCharArray();
+
+        for (int i = 0; i < chars.length-1; i++) {
+            ch = chars[i];
+            if (String.valueOf(chars[i+1]).matches("[0-9]")){
+                for (int j = 0; j < Character.digit(chars[i+1], 10); j++) {
+                    stringBuilder.append(ch);
+                }
+                i++;
+            } else {
+                stringBuilder.append(ch);
+            }
+        }
+
+        System.out.println(stringBuilder.toString());
     }
 }
